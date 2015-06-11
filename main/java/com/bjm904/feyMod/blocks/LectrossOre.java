3ee2d38d0eeaf7591f0ef04089ef94add21ae654
Bryce Meyer <bjm904@gmail.com>
2015-06-10 22:44:44 -0700

    Increase lectross drop

--- main/java/com/bjm904/feyMod/blocks/LectrossOre.java
+++ main/java/com/bjm904/feyMod/blocks/LectrossOre.java
@@ -1,11 +1,16 @@
 package com.bjm904.feyMod.blocks;
 
+import java.util.Random;
+
 import com.bjm904.feyMod.help.Reference;
+import com.bjm904.feyMod.init.ModBlocks;
+import com.bjm904.feyMod.init.ModItems;
 import com.bjm904.feyMod.tabs.ModTabs;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
 public class LectrossOre extends Block{
 	public LectrossOre(){
@@ -14,8 +19,21 @@ public class LectrossOre extends Block{
     	setBlockTextureName(Reference.MODID+":"+getUnlocalizedName().substring(5));
     	setCreativeTab(ModTabs.tabFeyMod);
     	setStepSound(soundTypeStone);
-		setHardness(2.5F);
+		setHardness(3.0F);
 		setResistance(10.0F);
 		setHarvestLevel("pickaxe", 3);
+    }
+    public int quantityDropped(Random random)
+    {
+    	Random r = new Random();
+    	float chance = r.nextFloat();
+    	if (chance <= 0.25f) return 2;
+    	else return 1;
+    	
+    }
+
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+    {
+        return ModItems.lectross;
     }
 }
